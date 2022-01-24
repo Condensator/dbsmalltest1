81ec1a574e85c742e2203a237bc74ed5de7132bb
Condensator <email>
2022-01-24 12:05:44 +0200

    initial

---
+++ dbo/Tables/Creation-Scripts/Table_1.sql
@@ -0,0 +1,11 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Table_1](
+	[sdf] [nchar](10) COLLATE Latin1_General_CI_AS NULL,
+	[sdfdfs] [nchar](10) COLLATE Latin1_General_CI_AS NULL,
+	[sdfsdfsdf] [nchar](10) COLLATE Latin1_General_CI_AS NULL
+)
+
+GO
